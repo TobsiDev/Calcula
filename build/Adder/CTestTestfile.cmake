c5b7b0fad9399190b539bfb798830ba2e540b1fa
Tobsi <tsh40884137@gmail.com>
2021-03-26 00:06:50 +0100

    Delete build directory

--- build/Adder/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tsh40/Desktop/Prog-C/Program/Adder
-# Build directory: C:/Users/tsh40/Desktop/Prog-C/Program/build/Adder
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tsh40/Desktop/Prog-C/Program
-# Build directory: C:/Users/tsh40/Desktop/Prog-C/Program/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Formulas")
